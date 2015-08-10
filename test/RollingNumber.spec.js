64fdf984da2396822cc56d337485a1ba50fd1675
Igor Sechyn <isechyn@atlassian.com>
2015-08-10 11:20:25 +1000

    added more tests

--- test/RollingNumber.spec.js
+++ test/RollingNumber.spec.js
@@ -36,6 +36,22 @@ describe("RollingNumber", function() {
         }, 1500);
     });
 
+    it("should reset the window if no activity was reported for the period longer than the window itself", function(done) {
+        var underTest = new RollingNumber({timeInMillisecond: 1000, numberOfBuckets: 2});
+        var originalTimeout = jasmine.DEFAULT_TIMEOUT_INTERVAL;
+        jasmine.DEFAULT_TIMEOUT_INTERVAL = 10000;
+        underTest.increment(RollingNumberEvent.SUCCESS);
+        underTest.rollWindow(Date.now());
+        underTest.increment(RollingNumberEvent.SUCCESS);
+        expect(underTest.buckets.length).toBe(2);
+        setTimeout(function() {
+            underTest.increment(RollingNumberEvent.SUCCESS);
+            expect(underTest.buckets.length).toBe(1);
+            jasmine.DEFAULT_TIMEOUT_INTERVAL = originalTimeout;
+            done();
+        }, 1001);
+    });
+
     it("should not exceed the max number of buckets", function() {
         var underTest = new RollingNumber({windowLength: 60000, numberOfBuckets: 2});
         underTest.rollWindow(new Date().getTime());
